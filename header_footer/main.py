2153f03c3cc58311b903b334056efa3e5cb9752c
G. Ramos <gramos@plux.info>
2020-01-10 13:25:22 +0000

    Task #1144377947524441/1147852392706609

--- header_footer/main.py
+++ header_footer/main.py
@@ -237,7 +237,7 @@ def _generate_post_build_files():
 
 # Execute Script.
 #run(list_notebooks=["eeg_extract_alphaband"])
-run(list_notebooks=["synchrony_sound"], exclude_notebooks=["hands_on_biostec", "hands_on_biostec_solutions"], signal_samples_flag=False, delete_old_files=False, new_notebooks_list=["synchrony_sound"])
+run(list_notebooks=["synchronisation"], exclude_notebooks=["hands_on_biostec", "hands_on_biostec_solutions"], signal_samples_flag=False, delete_old_files=True, new_notebooks_list=["synchrony_sound"])
 #run(exclude_notebooks=["hands_on_biostec", "hands_on_biostec_solutions"], signal_samples_flag=False)
 
 # 29/11/2018  17h18m :)
