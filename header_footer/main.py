a38107eb7a88fddc5be7efabcbd5088030208fa4
G. Ramos <gramos@plux.info>
2020-06-15 18:41:56 +0100

    Repository Update related with 15th June release

--- header_footer/main.py
+++ header_footer/main.py
@@ -238,8 +238,8 @@ def _generate_post_build_files():
     post_build_file.close()
 
 # Execute Script.
-run(list_notebooks=["temporal_statistical_parameters"], exclude_notebooks=["hands_on_biostec", "hands_on_biostec_solutions"],
+run(list_notebooks=["snr_determination"], exclude_notebooks=["hands_on_biostec", "hands_on_biostec_solutions"],
     signal_samples_flag=False, delete_old_files=False, delete_base_files=True,
-    new_notebooks_list=["digital_filtering_filtfilt", "temporal_statistical_parameters"])
+    new_notebooks_list=["digital_filtering_filtfilt", "temporal_statistical_parameters", "snr_determination"])
 
 # 29/11/2018  17h18m :)
