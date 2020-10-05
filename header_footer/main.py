190696c15bf8f412bf3dc7cbe0d6dbb26e0a3da1
G. Ramos <gramos@plux.info>
2020-10-05 21:04:21 +0100

    Fixing graphical issue with the latest release

--- header_footer/main.py
+++ header_footer/main.py
@@ -238,9 +238,9 @@ def _generate_post_build_files():
     post_build_file.close()
 
 # Execute Script.
-run(list_notebooks=["activity_distinction", "android_bitalino_sync", "android_file_sync"],
+run(list_notebooks=["activity_distinction"],
     exclude_notebooks=["hands_on_biostec", "hands_on_biostec_solutions"],
-    signal_samples_flag=False, delete_old_files=False, delete_base_files=False,
+    signal_samples_flag=False, delete_old_files=True, delete_base_files=False,
     new_notebooks_list=["activity_distinction"])
 
 # 29/11/2018  17h18m :)
