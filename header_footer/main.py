9d92e042cdc162a8c5fe844b282f641c48e8dccc
G. Ramos <gramos@plux.info>
2021-02-26 16:57:05 +0000

    Publishing Notebook updated by @p-probst

--- header_footer/main.py
+++ header_footer/main.py
@@ -238,7 +238,7 @@ def _generate_post_build_files():
     post_build_file.close()
 
 # Execute Script.
-run(list_notebooks=["eeg_detect_erp"],
+run(list_notebooks=["android_signal_resampling"],
     exclude_notebooks=["hands_on_biostec", "hands_on_biostec_solutions"],
     signal_samples_flag=False, delete_old_files=True, delete_base_files=False,
     new_notebooks_list=["rip_science_hour"])
